144eb5f6b3970b0f06950e68703a0ddd75179510
Anna Sakurai <zenasak@gmail.com>
2022-07-23 13:52:57 +0200

    connect to client using socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-
 const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = 5050;
+
+// communicate with client side
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on * "disconnect",
+    () => {
+      console.log("user disconnected");
+    };
+});
 
 server.listen(PORT, () => console.log(`server is running on ${PORT}`));
